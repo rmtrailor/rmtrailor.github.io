e3603e5614bc5ffcf94d23340fb724adcab1453b
Rubin Trailor <rubtrail@gmail.com>
2024-10-04 13:33:03 -0700

    Clean up old files and script

--- js/index.js
+++ js/index.js
@@ -236,39 +236,3 @@ document.addEventListener("scroll", (e) => {
   // carousel infinitely-scrolling.
   slideWrapper.classList.add("smooth-scroll");
 })();
-
-// Navbar setup
-// const navToggle = document.querySelector('.nav-toggle');
-// const navLinks = document.querySelectorAll('.mobile-nav__link');
-
-// navToggle.addEventListener('click', () => {
-//   document.body.classList.toggle('nav-open');
-// });
-
-// navLinks.forEach((link) => {
-//   link.addEventListener('click', () => {
-//     document.body.classList.remove('nav-open');
-//   });
-// });
-
-// // Skill Modals Setup
-// const portfolioModals = document.querySelectorAll('.portfolio-modal');
-// const portfolioButtons = document.querySelectorAll('.portfolio__item');
-
-// portfolioButtons.forEach((portfolioItem) => {
-//   portfolioItem.addEventListener('click', (event) => {
-//     const portfolioName = event.target.name;
-//     const targetModal = document.querySelector(
-//       `[portfolio-name="${portfolioName}"]`
-//     );
-//     targetModal.classList.toggle('modal-open');
-//   });
-// });
-
-// // Called by the close triggers inside the portfolio modals
-// function closeModal(portfolioName) {
-//   const targetModal = document.querySelector(
-//     `[portfolio-name="${portfolioName}"]`
-//   );
-//   targetModal.classList.remove('modal-open');
-// }
